b0c099f1017741e07b45c5428556f2b2e7567f4e
Vlas98 <98698845+Vlas98@users.noreply.github.com>
2023-01-10 00:26:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <body>
 
          <form action="index.php" method="post">
-            <p> Name: <input type="text" name="name"/></p>
+            <p> Name: <textarea name="name"/></textarea></p>
             <p><input type="submit"/> </p>
          </form>
         
