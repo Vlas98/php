bb1143ba02648b0ef924fa4a48cb3b1f4f430ab5
Vlas98 <98698845+Vlas98@users.noreply.github.com>
2023-01-10 00:41:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,16 @@
 
     <body>
 
-         <form action="index.php" method="post">
-            <p> Name: <textarea name="name"/></textarea></p>
-            <p><input type="submit"/> </p>
+
+         <form action="template.phtml" method="post">
+            <p> Name: <textarea name="name"></textarea></p>            
+            <p><input type="submit"> </p>
          </form>
         
-        <?php
-        
 
-            if(empty($_POST['name']))
-            {
-                        echo 'Имя!';
-
-            } 
-            else
-            {
-                echo htmlspecialchars($_POST['name']);
-            }
-        ?>
     </body>
 </html>
+<?php 
+include 'template.phtml';
+
+ ?>
