8704ecc3d4353a2bbc81ee7b044007dd39383ba5
Vlas98 <stepanov.vlas@yandex.ru>
2023-01-23 19:20:12 +0300

    save name of author with text

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php    
   
 $name = $_POST['name'] ?? '';
-include "./Templates/template.phtml";
+$author = $_POST['author'] ?? '';
 
+$nameListPath = 'list.json';
+$json = file_get_contents($nameListPath);
+
+if(!empty($json)){
+$nameList = json_decode($json, true);
+}else{
+$nameList= [];
+}
+
+if(!empty($name) /*&& !empty($author) */)
+{
+$nameList[] = ["author" =>$author, "name" => $name];
+file_put_contents('list.json',json_encode($nameList));
+}
+
+include "./Templates/template.phtml";
