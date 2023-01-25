0ddf76b12b1246d4316741432f7cf466da47cbe2
Vlas98 <stepanov.vlas@yandex.ru>
2023-01-25 20:19:51 +0300

    show message for last 5 minuts

--- index.php
+++ index.php
@@ -20,4 +20,17 @@ $nameList[] = ["data" => $data,"author" =>$author, "name" => $name];
 file_put_contents('list.json',json_encode($nameList));
 }
 
+
+$newComments = array_filter($nameList, function($item){
+    $dataComment = $item['data'];
+    $dataComment = strtotime($dataComment);
+    $now = strtotime(date('jS F Y h:i:s'));
+
+    $different = ($now - $dataComment);
+    $division = intdiv($different, 300);
+
+    return $division <= 5;
+});
+
+
 include "./Templates/template.phtml";
