463a2b52374c95280efb868f336da59a1df63295
Vlas98 <stepanov.vlas@yandex.ru>
2023-02-11 22:18:19 +0300

    add routing

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php    
 
-define('APP_PATH',__DIR__);// глобальная константа пути к корню директории (Для чего)
+define('APP_PATH', __DIR__);// глобальная константа пути к корню директории (Для чего)
+
 
 include_once APP_PATH . '/src/AuthGateway.php'; // подключаем авторизацию (не авторизацию, а файл с классом для работы с авторизацией)
 
 $authGateway = new AuthGateway($_COOKIE); //  создаем новый экземпляр клааса авторизации
 
-if($authGateway->isAuth()){// проверяем на авторизацию
-    include APP_PATH . '/src/chat.php'; // если авторизовани - подключаем чат
-}else{
-    include APP_PATH . '/src/login.php';// иначе( если не авторизован) подключаем форму авторизации
-}
 
+// routing
+if($_SERVER['REQUEST_URI'] === '/logout')
+{
+    include APP_PATH . '/logout.php';    
+}else{
+    if($authGateway->isAuth()){// проверяем на авторизацию
+        include APP_PATH . '/src/chat.php'; // если авторизовани - подключаем чат
+    }else{
+        include APP_PATH . '/src/login.php';// иначе( если не авторизован) подключаем форму авторизации
+    }
+}
